9d8f8281903a0d76c3d72e233c29fdb9b10a68bc
pavzari <(none)>
2023-03-28 17:53:01 +0100

    added readme and option for single or multiple ticketsand target emails

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ra_ticket_watcher
+This script monitors the Resident Advisor (RA) event page for resell tickets and sends an email notification when any of the desired tickets become available. Uses Gmail's SMTP server.
+
+## Usage
+
+1. Clone this repository.
+2. Install the required packages.
+3. Edit the following variables in the `ra-ticket-watcher.py` file or set them as environment variables:
+- `URL`: The URL of the RA event page.
+- `TICKETS_WANTED`: Price of currently soldout ticket (or a list of tickets) you are interested in. (e.g.,`"£15.00"` or `["£15.00", "£17.00", "£20.00"]`)
+- `TARGET_EMAIL`: The email address (or a list of addresses) to send the notification to.
+- `GMAIL_CLIENT_EMAIL`: 
+- `GMAIL_CLIENT_PASS`: Generate an App password for Gmail SMTP authentication.
+4. Create a cron job to run `ra_ticket_scraper.py` automatically. 
+
+## GitHub Actions
+
+This repository includes a GitHub Actions pipeline that runs the script as a cron job. To use the pipeline:
+
+1. Fork this repository.
+2. Add your Gmail credentials and target emails as secrets in your GitHub repository: `GMAIL_CLIENT_EMAIL`, `GMAIL_CLIENT_PASS`, `TARGET_EMAILS`.
+3. Set the rest of the variables inside `ra-ticket-watcher.py` file or add them as secrets.
+4. Uncomment and edit the schedule part in the `.github/workflows/ra-ticket-watcher.yml` file.
+
--- ra-ticket-watcher.py
+++ ra-ticket-watcher.py
@@ -4,11 +4,9 @@ soldout tickets and checks them against a list of wanted tickets. Notifies via
 Gmail if any become available. 
 """
 # TODO:
-# 1. Remove hardcoding of URL, TARGET_EMAIL, TICKETS_WANTED, TICKET_WANTED
-# 2. Add support for multiple events
-# 3. Add support for multiple target emails
-# 4. Requirements.txt
-# 5. Update actions pipeline.
+#  Add support for multiple events.
+#  Requirements.txt
+#  Avoid sending multiple email notifications in case multiple ticket types become available at the same time.
 
 import os
 import ssl
@@ -21,52 +19,58 @@ HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 REQUEST_TIMEOUT = 15
 EMAIL_SUBJECT = "RA tickets are available"
 EMAIL_CONTENT = f"Quick, tickets are now available here: {EVENT_URL}"
-TICKETS_WANTED = os.environ["TICKETS_WANTED"] #['£15.00', '£17.00', '£20.00']
-TARGET_EMAIL = os.environ["TARGET_EMAIL"]
+TICKETS_WANTED = os.environ["TICKETS_WANTED"] 
+TARGET_EMAILS = os.environ["TARGET_EMAILS"]
 GMAIL_CLIENT_EMAIL = os.environ["GMAIL_CLIENT_EMAIL"]
 GMAIL_CLIENT_PASS = os.environ["GMAIL_CLIENT_PASS"]
 
 
 class GmailClient:
-  def __init__(self):
-      self.port = 465
-      self.smtp_server_domain_name = "smtp.gmail.com"
-      self.sender_email = GMAIL_CLIENT_EMAIL
-      self.password = GMAIL_CLIENT_PASS
+    def __init__(self):
+        self.port = 465
+        self.smtp_server_domain_name = "smtp.gmail.com"
+        self.sender_email = GMAIL_CLIENT_EMAIL
+        self.password = GMAIL_CLIENT_PASS
 
-  def send(self, target_email, email_subject, email_content):
-    try:
-      context = ssl.create_default_context()
-      with smtplib.SMTP_SSL(self.smtp_server_domain_name, self.port, context=context) as connection:
-        connection.login(self.sender_email, self.password)
-        connection.sendmail(self.sender_email, target_email, f"Subject: {email_subject}\n\n{email_content}")
-        connection.quit()
-    except smtplib.SMTPException as e:
-        raise SystemExit(e)
+    def send(self, target_email, email_subject, email_content):
+      try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.smtp_server_domain_name, self.port, context=context) as connection:
+          connection.login(self.sender_email, self.password)
+          connection.sendmail(self.sender_email, target_email, f"Subject: {email_subject}\n\n{email_content}")
+          connection.quit()
+      except smtplib.SMTPException as e:
+          raise SystemExit(e)
     
 
 def scrape_tickets():
-  soldout_tickets = []
-  try:
-    page = requests.get(EVENT_URL, headers=HEADERS, timeout=REQUEST_TIMEOUT)
-    page.raise_for_status()
-  except requests.exceptions.RequestException as e: 
-    raise SystemExit(e)
+    soldout_tickets = []
+    try:
+      page = requests.get(EVENT_URL, headers=HEADERS, timeout=REQUEST_TIMEOUT)
+      page.raise_for_status()
+    except requests.exceptions.RequestException as e: 
+      raise SystemExit(e)
 
-  soup = BeautifulSoup(page.content, "html.parser")   
-  for ticket in soup.find_all("li", class_= "closed"):
-      price = ticket.find("div", class_="type-price")
-      soldout_tickets.append(price.text)
-  return soldout_tickets
+    soup = BeautifulSoup(page.content, "html.parser")   
+    for ticket in soup.find_all("li", class_= "closed"):
+        price = ticket.find("div", class_="type-price")
+        soldout_tickets.append(price.text)
+    return soldout_tickets
 
 
 def check_tickets_and_notify():
-  soldout_tickets = scrape_tickets()
-  for ticket in TICKETS_WANTED:
-    if ticket not in soldout_tickets:
-      email = GmailClient()
-      email.send(TARGET_EMAIL, EMAIL_SUBJECT, EMAIL_CONTENT)
+    soldout_tickets = scrape_tickets()
+    email = GmailClient()
+    if isinstance(TARGET_EMAILS, str):
+        TARGET_EMAILS = [TARGET_EMAILS]
+    if isinstance(TICKETS_WANTED, str):
+        TICKETS_WANTED = [TICKETS_WANTED]
+
+    for ticket in TICKETS_WANTED:
+        if ticket not in soldout_tickets:
+          for TARGET_EMAIL in TARGET_EMAILS:
+            email.send(TARGET_EMAIL, EMAIL_SUBJECT, EMAIL_CONTENT)
 
 if __name__ == '__main__':
-  check_tickets_and_notify()
+    check_tickets_and_notify()
 
